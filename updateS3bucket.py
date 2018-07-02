abcefca39ae024fccde50b96f7c06fccd7e91a17
erodium <liamhughes22@hotmail.com>
2018-07-01 17:40:41 -0700

    made it more lambda friendly with logger

--- updateS3bucket.py
+++ updateS3bucket.py
@@ -1,11 +1,11 @@
 import boto3
 from datetime import datetime
+#from botocore.vendored import requests
 import requests
 import pytz
 from pytz import timezone
 import logging
 
-logging.basicConfig(level=logging.INFO)
 NA_TOURN_ID="8531db79-ade3-4294-ae4a-ef639967c393" #should go away when this supports multiple tournaments
 NA_LCS_TEAMS = [
         {"id":18, "slug":"cloud9"},
@@ -25,15 +25,17 @@ SCHEDULED_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 ITEM_TEMPLATE= {"updatedAt":"","slug":"","nextMatch":""}
 DYNAMODB_TABLE_NAME="teams_na-lcs-id"
 
-
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(DYNAMODB_TABLE_NAME)
 
 
 def lambda_handler(event, context):
+    logger = logging.getLogger()
+    logger.setLevel(level=logging.INFO)
+    print("Update requested at " + event['time'])
     overallStatus = 0
     for team in NA_LCS_TEAMS:
-        logging.info("starting %s", team['slug'])
+        logger.info("starting %s", team['slug'])
         APIteamData = getTeam(team['slug'], NA_TOURN_ID)
         APInextScheduledItem = APIteamData['scheduleItems'][0]
         APIteams = APIteamData['teams']
@@ -41,10 +43,9 @@ def lambda_handler(event, context):
         APInsiTimeStr = APInextScheduledItem['scheduledTime'][:-5]
         DBteamData = table.get_item(Key={"id":team['id']})
         if 'Item' in DBteamData:
-            logging.info("item found")
             DBnextMatchTimeStr = DBteamData['Item']['nextMatch']
             if dataHasBeenUpdated(APInsiTimeStr,DBnextMatchTimeStr):
-                logging.info("data had been updated")
+                logger.info("Updating %s from %s to %s", team['slug'], APInsiTimeStr, DBnextMatchTimeStr)
                 pdt = timezone('US/Pacific')
                 APInsiDTO = datetime.strptime(APInsiTimeStr, SCHEDULED_TIME_FORMAT)
                 utc_dt = pytz.utc.localize(APInsiDTO)
@@ -53,10 +54,11 @@ def lambda_handler(event, context):
                 updateExpression = "SET nextMatch = :m"
                 expressionAttributeValue = {":m": pdtNSIstr}
                 table.update_item(Key={"id":team['id']},UpdateExpression=updateExpression,ExpressionAttributeValues=expressionAttributeValue)
+                print(team['slug'])
             else:
-                logging.info("no changes made")
+                logger.info("No changes made to %s", team['slug'])
         else:
-            logging.info("no item found, creating")
+            logger.info("No item found for %s; creating", team['slug'])
             newItem = ITEM_TEMPLATE.copy()
             newItem['id']=APIcurrentTeam['id']
             newItem['updatedAt'] = APIcurrentTeam['updatedAt']
@@ -72,7 +74,6 @@ def lambda_handler(event, context):
 
 
 def dataHasBeenUpdated(APIupdatedAtStr, DBupdatedAtStr):
-    logging.info("checking data")
     TimeAPIupdated = datetime.strptime(APIupdatedAtStr, SCHEDULED_TIME_FORMAT)
     TimeDBupdated = datetime.strptime(DBupdatedAtStr, SCHEDULED_TIME_FORMAT)
     if TimeDBupdated < TimeAPIupdated:
@@ -81,6 +82,5 @@ def dataHasBeenUpdated(APIupdatedAtStr, DBupdatedAtStr):
 
 
 def getTeam(slug, tournamentId):
-    logging.info("Getting matches for %s in %s", slug, tournamentId)
     r = requests.get("https://api.lolesports.com/api/v1/teams?slug=" + str(slug) + "&tournament=" + str(tournamentId)).json()
     return r
